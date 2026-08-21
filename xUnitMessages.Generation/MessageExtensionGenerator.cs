1a9ea2a67e4d31d641ce8ec25a09acb9dad994df
Christoph Hornung <christoph.hornung@crosberg.de>
2026-08-21 11:47:16 +0200

    Cleanup generation

--- xUnitMessages.Generation/MessageExtensionGenerator.cs
+++ xUnitMessages.Generation/MessageExtensionGenerator.cs
@@ -84,8 +84,8 @@ public static partial class AssertM
 			   m.IsDefinition &&
 			   m.DeclaredAccessibility == Accessibility.Public &&
 			   m.Name != "Equals" &&
-			   !m.Name.StartsWith("Override") &&
 			   m.Name != "ReferenceEquals" &&
+			   !m.Name.StartsWith("Override") &&
 			   m.GetAttributes()
 				   .All(a => a.AttributeClass?.Name.Contains("ObsoleteAttribute") != true);
 	}
@@ -107,6 +107,7 @@ public static partial class AssertM
 		return member.Name != "False" &&
 			   member.Name != "True" &&
 			   member.Name != "Fail" &&
+			   member.Name != "Skip" &&
 			   member.Name != "SkipUnless" &&
 			   member.Name != "SkipWhen";
 	}
@@ -431,7 +432,7 @@ public static partial class AssertM
 
 	private string? GetDefaultValue(IParameterSymbol symbol)
 	{
-		if (symbol.HasExplicitDefaultValue == false)
+		if (!symbol.HasExplicitDefaultValue)
 		{
 			return null;
 		}
@@ -443,7 +444,7 @@ public static partial class AssertM
 
 		if (symbol.ExplicitDefaultValue is string stringValue)
 		{
-			return $"\"{stringValue.Replace("\\", "\\\\").Replace("\"", "\\\"")}\"";
+			return $"\"{stringValue.Replace(@"\", @"\\").Replace(@"""", @"\""")}\"";
 		}
 
 		if (symbol.ExplicitDefaultValue is bool value)
